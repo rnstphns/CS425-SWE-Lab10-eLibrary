87e64f7af8579a3d212eb00db432171e25963592
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-07-06 10:33:23 -0500

    initialize SpringBoot project

---
+++ src/main/java/edu/miu/cs/cs425/elibrary/ELibraryRyanStephensApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryRyanStephensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryRyanStephensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/cs/cs425/elibrary/ELibraryRyanStephensApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryRyanStephensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
